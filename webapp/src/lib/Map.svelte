934df6081cca220c2e1ed893ebe6266b1a9fa357
Alex Lin <38541529+alxn3@users.noreply.github.com>
2025-02-02 01:14:55 -0500

    Map cool

--- webapp/src/lib/Map.svelte
+++ webapp/src/lib/Map.svelte
@@ -1,36 +1,37 @@
 <script lang="ts">
-  import { Deck, FlyToInterpolator } from '@deck.gl/core';
-  import type { MapViewState, ViewStateMap } from '@deck.gl/core';
-  import { GeoJsonLayer, ScatterplotLayer } from '@deck.gl/layers';
+  import { Deck, FlyToInterpolator, LinearInterpolator } from '@deck.gl/core';
+  import type { MapViewState, PickingInfo, ViewStateMap } from '@deck.gl/core';
+  import { GeoJsonLayer, PolygonLayer, ScatterplotLayer, TextLayer } from '@deck.gl/layers';
   // import { DataFilterExtension } from '@deck.gl/extensions';
   // import type { DataFilterExtensionProps } from '@deck.gl/extensions';
+
+  import { nameToAbbrev } from './name_to_abbrev';
 
   import CountyGeoJson from '$lib/data/counties.geojson?url';
   import StateGeoJson from '$lib/data/states.geojson?url';
   import type { Feature, FeatureCollection, Geometry, MultiPolygon } from 'geojson';
-  import { onMount } from 'svelte';
+
+  import Color from 'color';
+
+  let tooltip: HTMLDivElement;
+
+  let {
+    countyData = $bindable(),
+  }: {
+    countyData: {
+      county: string;
+      state: string;
+    };
+  } = $props();
 
   const INITIAL_VIEW_STATE: MapViewState = {
     latitude: 40,
     longitude: -96,
     zoom: 4,
+    minZoom: 3.5,
+    maxZoom: 8,
   };
-
-  // fetch from /add endpoint and print
-  // fetch('/add', {state: Virginia}}) on Mount
-
-  onMount(async () => {
-    const response = await fetch('/api/add', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ state: 'alaska' }),
-    });
-    const data = await response.json();
-    console.log(data);
-  });
-
+  
   type StatePropertiesTypes = {
     geo_point_2d: { lon: number; lat: number };
     year: string;
@@ -57,6 +58,32 @@
     coty_gnis_code: string;
   };
 
+  let tooltipText: string = $state('');
+
+  const updateTooltip = ({
+    object,
+    x,
+    y,
+  }: PickingInfo<Feature<Geometry, CountyPropertiesTypes>>) => {
+    if (object) {
+      tooltip.style.display = 'block';
+      tooltip.style.left = `${x}px`;
+      tooltip.style.transform = 'translate(-50%, -120%)';
+      tooltip.style.top = `${y}px`;
+      tooltipText = object.properties.coty_name_long[0];
+    } else {
+      tooltip.style.display = 'none';
+    }
+  };
+
+  let selectedState: string | null = null;
+
+  const clearSelected = () => {
+    selectedState = null;
+    tooltip.style.display = 'none';
+    countyData = { county: '', state: '' };
+  };
+
   $effect(() => {
     (async () => {
       const state_data: FeatureCollection<MultiPolygon, StatePropertiesTypes> = await (
@@ -74,6 +101,78 @@
         new Map<string, { lon: number; lat: number }>(),
       );
 
+      const stateCountyLayerMap: Map<
+        string,
+        GeoJsonLayer<CountyPropertiesTypes>
+      > = state_data.features.reduce((acc, d) => {
+        const stateCode = d.properties.ste_code[0];
+        acc.set(
+          stateCode,
+          new GeoJsonLayer<CountyPropertiesTypes>({
+            id: `counties-layer-${stateCode}`,
+            data: county_data.features.filter((d) => d.properties.ste_code[0] === stateCode),
+            opacity: 1,
+            pickable: false,
+            autoHighlight: true,
+            highlightColor: [255, 255, 255, 128],
+            stroked: true,
+            extruded: false,
+            filled: true,
+            getLineWidth: 20,
+            lineWidthMinPixels: 1,
+            getPointRadius: 100,
+            getFillColor: () =>
+              colorLow.lch().mix(colorHigh.lch(), Math.random()).rgb().array() as any,
+            getLineColor: [255, 255, 255],
+            transitions: {
+              opacity: {
+                duration: 300,
+                easing: (x: number) => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2),
+              },
+            },
+            onHover: updateTooltip,
+            onDrag: updateTooltip,
+            onClick: (pickable: PickingInfo<Feature<Geometry, CountyPropertiesTypes>>) => {
+              let object = pickable.object;
+              if (object) {
+                countyData.county =
+                  countyData.county === object.properties.coty_name_long[0]
+                    ? ''
+                    : object.properties.coty_name_long[0];
+
+                countyData.state = nameToAbbrev[object.properties.ste_name[0]];
+
+                let coord = countyData.county
+                  ? object.properties.geo_point_2d
+                  : stateCenterMap.get(object.properties.ste_code[0]) ||
+                    object.properties.geo_point_2d;
+
+                const dist = Math.sqrt(
+                  (coord.lon - vs.longitude) ** 2 + (coord.lat - vs.latitude) ** 2,
+                );
+                deckInstance.setProps({
+                  initialViewState: {
+                    ...INITIAL_VIEW_STATE,
+                    longitude: coord.lon + (Math.random() - 0.5) / 100,
+                    latitude: coord.lat,
+                    zoom: countyData.county ? 7 : 5.5,
+                    transitionDuration: dist * 200,
+                    transitionEasing: (x: number) =>
+                      x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2,
+                    transitionInterpolator: new LinearInterpolator([
+                      'zoom',
+                      'latitude',
+                      'longitude',
+                    ]),
+                  },
+                });
+              }
+            },
+          }),
+        );
+        return acc;
+      }, new Map<string, GeoJsonLayer<CountyPropertiesTypes>>());
+
       const stateLayer = new GeoJsonLayer<StatePropertiesTypes>({
         id: 'states-layer',
         data: state_data,
@@ -86,39 +185,26 @@
         getFillColor: [255, 100, 100],
       });
 
-      let selectedState: string = 'null';
+      const colorLow = new Color(
+        window.getComputedStyle(document.documentElement).getPropertyValue('--color-low'),
+      );
+      const colorHigh = new Color(
+        window.getComputedStyle(document.documentElement).getPropertyValue('--color-high'),
+      );
 
-      const stateOutlineLayer = new GeoJsonLayer<StatePropertiesTypes>({
-        id: 'states-outline-layer',
+      const stateFillLayer = new GeoJsonLayer<StatePropertiesTypes>({
+        id: 'states-fill-layer',
+        opacity: 1,
         data: state_data,
-        pickable: true,
+        pickable: false,
         stroked: true,
         extruded: false,
         filled: true,
         getLineWidth: 20,
         lineWidthMinPixels: 1,
-        getFillColor: [255, 100, 100],
+        getFillColor: () => colorLow.lch().mix(colorHigh.lab(), Math.random()).rgb().array() as any,
         getLineColor: [255, 255, 255],
-        autoHighlight: true,
-        highlightColor: [255, 255, 255, 128],
-        onClick: ({ object }) => {
-          selectedState = object.properties.ste_code[0];
-          deckInstance.setProps({
-            initialViewState: {
-              ...INITIAL_VIEW_STATE,
-              longitude: object.properties.geo_point_2d.lon,
-              latitude: object.properties.geo_point_2d.lat,
-              zoom: 5.5,
-              transitionInterpolator: new FlyToInterpolator({ speed: 2 }),
-              transitionDuration: 'auto',
-            },
-          });
-        },
         transitions: {
-          lineWidthMinPixels: {
-            duration: 300,
-            easing: (x: number) => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2),
-          },
           getFillColor: {
             duration: 300,
             easing: (x: number) => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2),
@@ -126,38 +212,81 @@
         },
       });
 
-      const stateCountyLayerMap: Map<
-        string,
-        GeoJsonLayer<CountyPropertiesTypes>
-      > = state_data.features.reduce((acc, d) => {
-        const stateCode = d.properties.ste_code[0];
-        acc.set(
-          stateCode,
-          new GeoJsonLayer<CountyPropertiesTypes>({
-            id: `counties-layer-${stateCode}`,
-            data: county_data.features.filter((d) => d.properties.ste_code[0] === stateCode),
-            opacity: 1,
-            pickable: true,
-            autoHighlight: true,
-            highlightColor: [255, 255, 255, 128],
-            stroked: true,
-            extruded: false,
-            filled: true,
-            getLineWidth: 20,
-            lineWidthMinPixels: 1,
-            getPointRadius: 100,
-            getFillColor: [255, 100, 100],
-            getLineColor: [255, 200, 200],
-            transitions: {
-              opacity: {
-                duration: 300,
-                easing: (x: number) => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2),
-              },
+      const stateTextLayer = new TextLayer<StatePropertiesTypes>({
+        id: 'states-text-layer',
+        data: state_data.features.map((d) => d.properties),
+        pickable: false,
+        getPosition: (d) => [d.geo_point_2d.lon, d.geo_point_2d.lat],
+        getText: (d) => nameToAbbrev[d.ste_name[0]],
+        getSize: 24,
+        getColor: [255, 255, 255],
+        sizeScale: 0.5,
+        getAngle: 0,
+        getTextAnchor: 'middle',
+        getAlignmentBaseline: 'center',
+        getPixelOffset: [0, 0],
+        transitions: {
+          getSize: {
+            duration: 300,
+            easing: (x: number) => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2),
+          },
+        },
+      });
+
+      let transitionDone = false;
+      let transitionTimeout: NodeJS.Timeout | null = null;
+
+      const stateOutlineLayer = new GeoJsonLayer<StatePropertiesTypes>({
+        id: 'states-outline-layer',
+        opacity: 1,
+        data: state_data,
+        pickable: true,
+        stroked: true,
+        filled: true,
+        getLineWidth: 20,
+        lineWidthMinPixels: 1,
+        getLineColor: [255, 255, 255],
+        getFillColor: [0, 0, 0, 0],
+        autoHighlight: true,
+        highlightColor: [255, 255, 255, 128],
+        onClick: ({ object }) => {
+          selectedState = object.properties.ste_code[0];
+          const dist = Math.sqrt(
+            (object.properties.geo_point_2d.lon - vs.longitude) ** 2 +
+              (object.properties.geo_point_2d.lat - vs.latitude) ** 2,
+          );
+          if (transitionTimeout) {
+            clearTimeout(transitionTimeout);
+          }
+          transitionDone = false;
+          transitionTimeout = setTimeout(() => {
+            transitionDone = true;
+            transitionTimeout = null;
+          }, dist * 50);
+          countyData = { county: '', state: '' };
+          deckInstance.setProps({
+            initialViewState: {
+              ...INITIAL_VIEW_STATE,
+              longitude: object.properties.geo_point_2d.lon + (Math.random() - 0.5) / 100,
+              latitude: object.properties.geo_point_2d.lat,
+              zoom: 5.5,
+              transitionDuration: dist * 50,
+              transitionEasing: (x: number) =>
+                x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2,
+              transitionInterpolator: new LinearInterpolator(['zoom', 'latitude', 'longitude']),
             },
-          }),
-        );
-        return acc;
-      }, new Map<string, GeoJsonLayer<CountyPropertiesTypes>>());
+          });
+        },
+        onDragStart: () => {
+          clearSelected();
+        },
+        transitions: {
+          lineWidthMinPixels: {
+            duration: 300,
+            easing: (x: number) => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2),
+          },
+        },
+      });
 
       // console.log(...stateCountyLayerMap.values());
 
@@ -165,8 +294,8 @@
       let preload = new Map<string, GeoJsonLayer<CountyPropertiesTypes>>();
 
       const loadStates = (viewState: MapViewState) => {
-        const max_dist = (6 - Math.min(Math.max(viewState.zoom, 4), 6)) ** 2 * 50 + 15;
-        const preload_dist = max_dist + 10;
+        const max_dist = (6 - Math.min(Math.max(viewState.zoom, 4.75), 5.75)) ** 2 * 50 + 15;
+        const preload_dist = max_dist * 2;
 
         for (const e of visible) {
           const distance =
@@ -196,18 +325,93 @@
             if (!visible.has(stateCode)) {
               visible.set(stateCode, stateCountyLayerMap.get(stateCode)!.clone({ opacity: 1 }));
             }
-          } else if (distance < max_dist * 2) {
+          } else if (distance < preload_dist) {
             if (!preload.has(stateCode)) {
               preload.set(stateCode, stateCountyLayerMap.get(stateCode)!.clone({ opacity: 0 }));
             }
           }
         }
+
+        visible.delete(selectedState || '');
       };
 
-      const updateLayers = (layers: GeoJsonLayer[]) => {
-        deckInstance.setProps({
-          layers,
+      let vs: MapViewState = INITIAL_VIEW_STATE;
+
+      const fullScreenPolygon = [
+        [-180, -90],
+        [180, -90],
+        [180, 90],
+        [-180, 90],
+      ];
+
+      const colorLayer = new PolygonLayer({
+        id: 'background',
+        opacity: 1,
+        data: [
+          {
+            polygon: fullScreenPolygon,
+          },
+        ],
+        getPolygon: (d) => d.polygon,
+        stroked: false,
+        filled: true,
+        pickable: false,
+        getFillColor: [32, 32, 128, 64],
+        transitions: {
+          opacity: {
+            duration: 100,
+            easing: (x: number) => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2),
+          },
+        },
+      });
+
+      const updateLayers = () => {
+        const colorLayerCopy = colorLayer.clone({
+          opacity: selectedState === null ? 0 : 1,
         });
+        if (vs.zoom > 4.75) {
+          deckInstance.setProps({
+            layers: [
+              stateLayer,
+              stateFillLayer,
+              stateTextLayer,
+              ...visible.values(),
+              ...preload.values(),
+              stateOutlineLayer.clone({
+                lineWidthMinPixels: 2,
+              }),
+              colorLayerCopy,
+              stateCountyLayerMap.get(selectedState || '')?.clone({
+                pickable: true,
+              }),
+            ],
+          });
+        } else {
+          deckInstance.setProps({
+            layers: [
+              stateLayer,
+              stateFillLayer,
+              stateTextLayer,
+              ...[...visible.values()].map((e) =>
+                e.clone({
+                  opacity: 0,
+                  pickable: false,
+                }),
+              ),
+              ...[...preload.values()].map((e) =>
+                e.clone({
+                  opacity: 0,
+                  pickable: false,
+                }),
+              ),
+              stateOutlineLayer,
+              colorLayerCopy,
+              stateCountyLayerMap.get(selectedState || '')?.clone({
+                pickable: true,
+              }),
+            ],
+          });
+        }
       };
 
       loadStates(INITIAL_VIEW_STATE);
@@ -215,24 +419,14 @@
         canvas: 'deck-container',
         initialViewState: INITIAL_VIEW_STATE,
         controller: true,
-        layers: [stateLayer, ...visible.values(), ...preload.values(), stateOutlineLayer],
+        layers: [stateLayer, ...visible.values(), ...preload.values(), stateFillLayer],
         onViewStateChange: ({ viewState }) => {
-          loadStates(viewState);
-          if (viewState.zoom > 4.5) {
-            updateLayers([
-              stateLayer,
-              ...visible.values(),
-              ...preload.values(),
-              stateOutlineLayer.clone({
-                lineWidthMinPixels: 2,
-                pickable: false,
-                autoHighlight: false,
-                getFillColor: [255, 100, 100, 0],
-              }),
-            ]);
-          } else {
-            updateLayers([stateLayer, ...visible.values(), ...preload.values(), stateOutlineLayer]);
+          vs = viewState;
+          if (transitionDone && viewState.zoom < 5.25) {
+            clearSelected();
           }
+          loadStates(viewState);
+          updateLayers();
         },
       });
     })();
@@ -241,4 +435,11 @@
 
 <div class="absolute left-0 top-0 h-full w-full">
   <canvas id="deck-container"></canvas>
+</div>
+
+<div
+  class="pointer-events-none absolute z-10 rounded-3xl bg-white px-2 py-1 text-xs text-gray-700"
+  bind:this={tooltip}
+>
+  {tooltipText}
 </div>
--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,53 +1,132 @@
 <script lang="ts">
   import Map from '$lib/Map.svelte';
+  import { AbbrevToName } from '$lib/name_to_abbrev';
 
   let search = '';
 
+  let countyData: {
+    county: string;
+    state: string;
+  } = $state({
+    county: '',
+    state: '',
+  });
+
   let tags: string[] = $state([]);
+
+  import { animate } from 'motion';
+
+  let infoDiv: HTMLDivElement;
+
+  const calcInfoBoxSize = () => {
+    if (window.innerWidth < 1024) {
+      if (countyData.county) {
+        animate(infoDiv, {
+          height: '20rem',
+          width: '100%',
+        });
+      } else {
+        animate(infoDiv, {
+          height: '0',
+          width: '100%',
+        });
+      }
+    } else {
+      if (countyData.county) {
+        animate(infoDiv, {
+          width: '32rem',
+          height: '100%',
+        });
+      } else {
+        animate(infoDiv, {
+          width: '0',
+          height: '100%',
+        });
+      }
+    }
+  };
+
+  $effect(() => {
+    calcInfoBoxSize();
+
+    if (countyData.county) {
+      (async () => {
+        console.log({ state: AbbrevToName[countyData.state] });
+        const response = await fetch('/api/add', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            state: AbbrevToName[countyData.state].toLowerCase(),
+            county: countyData.county.toLowerCase(),
+          }),
+        });
+        const data = await response.json();
+        console.log(data);
+      })();
+    }
+  });
 </script>
 
-<Map />
+<svelte:window onresize={calcInfoBoxSize} />
 
-<div class="absolutee left-0 top-0">
-  <h1>Hello</h1>
-</div>
-
-<div class="absolute right-0 top-0 m-4">
-  <div class="w-60 space-y-2 rounded-3xl bg-white p-2 shadow-md outline-1 outline-gray-100">
-    <div class="flex items-center gap-1">
-      <input
-        type="text"
-        class="w-full rounded-2xl border border-gray-200 p-2"
-        placeholder="Add a tag"
-        bind:value={search}
-        onkeydown={(e) => {
-          if (e.key === 'Enter') {
-            tags = [...tags, search];
-            search = '';
-            if (e.target instanceof HTMLInputElement) e.target.value = '';
-          }
-        }}
-      />
-      <button class="rounded-2xl bg-blue-500 p-2 text-white">Add</button>
-    </div>
-    {#each tags as tag}
-      <div
-        class="mt-2 flex items-center justify-between gap-2 border-t-[1px] border-gray-300 px-2 pt-2"
-      >
-        <span>{tag}</span>
-        <button class="rounded-lg bg-red-500 text-white">Remove</button>
+<div class="relative flex h-full w-full flex-col transition-all lg:flex-row">
+  <div class="relative h-full w-full">
+    <div class="relative left-0 top-0 h-full w-full">
+      <Map bind:countyData />
+      <div class="absolute right-0 top-0 m-4">
+        <div class="w-60 space-y-2 rounded-3xl bg-white p-2 shadow-md outline-1 outline-gray-100">
+          <div class="flex items-center gap-1">
+            <input
+              type="text"
+              class="w-full rounded-2xl border border-gray-200 p-2"
+              placeholder="Add a tag"
+              bind:value={search}
+              onkeydown={(e) => {
+                if (e.key === 'Enter') {
+                  tags = [...tags, search];
+                  search = '';
+                  if (e.target instanceof HTMLInputElement) e.target.value = '';
+                }
+              }}
+            />
+            <button class="rounded-2xl bg-blue-500 p-2 text-white">Add</button>
+          </div>
+          {#each tags as tag}
+            <div
+              class="mt-2 flex items-center justify-between gap-2 border-t-[1px] border-gray-300 px-2 pt-2"
+            >
+              <span>{tag}</span>
+              <button class="rounded-lg bg-red-500 text-white">Remove</button>
+            </div>
+          {/each}
+          {#if tags.length === 0}
+            <div class="text-center text-gray-500">No tags added</div>
+          {/if}
+        </div>
       </div>
-    {/each}
-    {#if tags.length === 0}
-      <div class="text-center text-gray-500">No tags added</div>
-    {/if}
+    </div>
+
+    <div
+      class="transform-[translateY(-50%)] pointer-events-none absolute left-0 top-[50%] m-4 flex h-[min(80%,40rem)] flex-col items-center rounded-2xl bg-white px-0.5 py-1 text-xs font-semibold text-gray-600 outline-1 outline-gray-300"
+    >
+      <p>100</p>
+      <div
+        class="bg-linear-to-t/oklab from-low to-high h-full w-8 rounded-lg p-2 shadow-md outline-1 outline-gray-100"
+      ></div>
+      <p>0</p>
+    </div>
+  </div>
+  <div bind:this={infoDiv} class="h-0 w-full grow-0 overflow-clip lg:h-full lg:w-0">
+    <div class="h-[20rem] w-full p-2 lg:h-full lg:w-[32rem]">
+      <h1 class="text-3xl font-bold text-gray-700">
+        {countyData.county}<span class="text-gray-500">, {countyData.state}</span>
+      </h1>
+    </div>
   </div>
 </div>
 
-<div
-  class="transform-[translateY(-50%)] pointer-events-none absolute left-0 top-[50%] m-4 h-[min(80%,40rem)]"
->
-  <div
-    class="h-full w-8 rounded-lg bg-gradient-to-t from-[rgb(255,0,0)] to-[rgb(0,255,0)] p-2 shadow-md outline-1 outline-gray-100"
-  ></div>
+<div class="absolute left-0 top-0">
+  <h1>{countyData.county}<span>{countyData.state}</span></h1>
 </div>
